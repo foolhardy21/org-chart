21837a31a9a8f8f50adad02b1ec16fb19a98dc9d
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-06-15 17:05:48 +0530

    feat - add a team

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,45 @@
 import { employees, teams } from "./data/org.data";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Modal from "./components/Modal";
 
+const ceo = employees.find(emp => emp.designation === 'CEO')
+const heads = employees.filter(emp => emp.designation === 'Head')
+
 function App() {
-  const ceo = employees.find(emp => emp.designation === 'CEO')
-  const heads = employees.filter(emp => emp.designation === 'Head')
+  const [allEmployees, setAllEmployees] = useState(employees)
+  const [allTeams, setAllTeams] = useState(teams)
   const [departmentTeams, setDepartmentTeams] = useState([])
-  const [currentTeam, setCurrentTeam] = useState('')
   const [currentDepartment, setCurrentDepartment] = useState('')
+  const [currentTeam, setCurrentTeam] = useState('')
   const [teamMembers, setTeamMembers] = useState([])
   const [modalType, setModalType] = useState('')
 
   function handleHeadClick(selectedDepartment) {
     setCurrentDepartment(selectedDepartment)
-    setDepartmentTeams(teams.filter(team => team.department === selectedDepartment))
+    setDepartmentTeams(allTeams.filter(team => team.department === selectedDepartment))
     setTeamMembers([])
   }
 
   function handleTeamClick(selectedTeam) {
     setCurrentTeam(selectedTeam)
-    setTeamMembers(employees.filter(emp => emp.team === selectedTeam))
+    setTeamMembers(allEmployees.filter(emp => emp.team === selectedTeam))
   }
 
   function handleAddMemberClick() {
     setModalType('add_member')
   }
+
+  function handleAddTeamClick() {
+    setModalType('add_team')
+  }
+
+  useEffect(() => {
+    setTeamMembers(allEmployees.filter(emp => emp.team === currentTeam))
+  }, [allEmployees])
+
+  useEffect(() => {
+    setDepartmentTeams(allTeams.filter(team => team.department === currentDepartment))
+  }, [allTeams])
 
   return (
     <div className='flx flx-column flx-center'>
@@ -49,6 +64,9 @@ function App() {
             <p>{team.department}</p>
           </article>)
         }
+        <article onClick={handleAddTeamClick} className="card-dim card-shadow-xs pd-md mg-xs">
+          <p>add a team</p>
+        </article>
       </section>
       <p className="mg-top-md mg-top-s">members</p>
       <section className="flx">
@@ -63,7 +81,7 @@ function App() {
         </article>
       </section>
       {
-        modalType.length > 0 && <Modal type={modalType} teamMembers={teamMembers} setTeamMembers={setTeamMembers} currentTeam={currentTeam} currentDepartment={currentDepartment} setModalType={setModalType} />
+        modalType.length > 0 && <Modal type={modalType} allEmployees={allEmployees} setAllEmployees={setAllEmployees} allTeams={allTeams} setAllTeams={setAllTeams} currentTeam={currentTeam} currentDepartment={currentDepartment} setModalType={setModalType} />
       }
     </div>
   );
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,8 +1,8 @@
 import { v4 as uuid } from 'uuid'
 
-const Modal = ({ type, teamMembers, setTeamMembers, currentTeam, currentDepartment, setModalType }) => {
+const Modal = ({ type, setAllEmployees, setAllTeams, currentTeam, currentDepartment, setModalType }) => {
 
-    // modal types - adding a member, adding a team, show employee details
+    // modal types - adding a member, edit member, adding a team, edit a team, show employee details
 
     function handleAddMember(e) {
         e.preventDefault()
@@ -12,12 +12,24 @@ const Modal = ({ type, teamMembers, setTeamMembers, currentTeam, currentDepartme
             phoneno: e.target.phoneno.value,
             email: e.target.email.value,
             team: currentTeam,
+            designation: 'Member',
             department: currentDepartment,
         }
-        setTeamMembers([...teamMembers, newMember])
+        setAllEmployees((allEmployees) => [...allEmployees, newMember])
         setModalType('')
     }
 
+    function handleAddTeam(e) {
+        e.preventDefault()
+        const newMember = {
+            _id: uuid(),
+            name: e.target.name.value,
+            department: currentDepartment,
+        }
+        setAllTeams((allTeams) => [...allTeams, newMember])
+        setModalType('')
+    }
+    console.log(type)
     return type === 'add_member' ? (
         <section className="flx flx-center modal-container pos-fixed tl-0 z-5">
             <article className="modal-md txt-primary pd-lg bg-primary">
@@ -25,6 +37,15 @@ const Modal = ({ type, teamMembers, setTeamMembers, currentTeam, currentDepartme
                     <input required type='text' placeholder="name" name='name' />
                     <input required type='number' placeholder="phone no." name='phoneno' />
                     <input required type='email' placeholder="email" name='email' />
+                    <button className="btn-solid pd-xs txt-md txt-ucase">add</button>
+                </form>
+            </article>
+        </section>
+    ) : type === 'add_team' ? (
+        <section className="flx flx-center modal-container pos-fixed tl-0 z-5">
+            <article className="modal-md txt-primary pd-lg bg-primary">
+                <form onSubmit={handleAddTeam}>
+                    <input required type='text' placeholder="name" name='name' />
                     <button className="btn-solid pd-xs txt-md txt-ucase">add</button>
                 </form>
             </article>

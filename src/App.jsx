027b65c5e20d5a10aca64b1a654c05e759feda64
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-06-16 15:43:19 +0530

    feat - search employees

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { employees } from "./data/org.data";
 import { useEffect, useState } from "react";
-import { CEOSection, HeadsSection, TeamsSection, MembersSection, Modal } from "./components";
+import { CEOSection, HeadsSection, TeamsSection, MembersSection, Modal, SearchSection } from "./components";
 import { useHeads, useTeams, useMembers } from "./contexts";
 
 function App() {
   const [allEmployees, setAllEmployees] = useState(employees)
   const [modalType, setModalType] = useState('')
+  const [searchedEmployees, setSearchedEmployees] = useState([])
   const { setHeadTeams, currentHead, setCurrentHead } = useHeads()
   const { currentTeam, allTeams, setCurrentTeam, setToBeEditedTeam } = useTeams()
   const { setTeamMembers, setToBeEditedMember } = useMembers()
@@ -51,15 +52,31 @@ function App() {
     setModalType('move_member')
     setToBeEditedMember(allEmployees.find(emp => emp._id === memberId))
   }
-  console.log(allEmployees)
+
+  function handleSearchQueryChange(e) {
+    if (e.target.value === '') {
+      setSearchedEmployees([])
+    } else if (isNaN(Number(e.target.value))) {
+      setSearchedEmployees(allEmployees.filter(emp => emp.name.toLowerCase().includes(e.target.value.toLowerCase()) || emp.email.toLowerCase().includes(e.target.value.toLowerCase())))
+    } else {
+      setSearchedEmployees(allEmployees.filter(emp => String(emp.phone).includes(e.target.value.toLowerCase())))
+    }
+  }
+
   return (
     <div className='flx flx-column flx-center'>
-      <CEOSection />
-      <HeadsSection handleHeadClick={handleHeadClick} />
-      <TeamsSection handleTeamClick={handleTeamClick} handleTeamEdit={handleTeamEdit} handleAddTeamClick={handleAddTeamClick} />
-      <MembersSection handleMemberEdit={handleMemberEdit} handleAddMemberClick={handleAddMemberClick} handleMemberMove={handleMemberMove} />
+      <input type='text' placeholder='search by name, email or phone' onChange={handleSearchQueryChange} />
       {
-        modalType.length > 0 && <Modal type={modalType} allEmployees={allEmployees} setAllEmployees={setAllEmployees} setModalType={setModalType} />
+        searchedEmployees.length > 0 ? <SearchSection searchedEmployees={searchedEmployees} />
+          : <>
+            <CEOSection />
+            <HeadsSection handleHeadClick={handleHeadClick} />
+            <TeamsSection handleTeamClick={handleTeamClick} handleTeamEdit={handleTeamEdit} handleAddTeamClick={handleAddTeamClick} />
+            <MembersSection handleMemberEdit={handleMemberEdit} handleAddMemberClick={handleAddMemberClick} handleMemberMove={handleMemberMove} />
+          </>
+      }
+      {
+        modalType.length > 0 && <Modal type={modalType} setAllEmployees={setAllEmployees} setModalType={setModalType} />
       }
     </div>
   );
@@ -68,4 +85,3 @@ function App() {
 export default App;
 
 // view detailed info
-// search members based on their details
